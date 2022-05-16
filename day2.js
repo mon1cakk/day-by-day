dce479aadb1b0255b3622555958063c9dcc78f80
lesliechen <435421355@qq.com>
2022-05-16 22:14:24 +0800

    feat:快乐数

---
+++ day2.js
@@ -0,0 +1,35 @@
+/**
+ *  快乐数
+ */
+
+ 示例:
+ 输入: 19
+ 输出: true
+//  解释:
+//  1^2 + 9^2 = 82
+//  8^2 + 2^2 = 68
+//  6^2 + 8^2 = 100
+//  1^2 + 0^2 + 0^2 = 1
+
+function handleFn(n, once) {
+  // 判断是否进入死循环
+  if(once[n]) {
+    return false
+  }
+  const list = n.toString().split('');
+  once[n] = true;
+  let result = 0;
+  list.forEach((item) => {
+    result += Math.pow(parseInt(item, 10), 2);
+  })
+  if(result === 1) {
+    return true
+  }else {
+    handleFn(result, once)
+  }
+}
+
+const isHappy = function(n) {
+  const once = {}
+  return handleFn(n, once)
+}
