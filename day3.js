add6acc8aca7d16f6650bf588cac4211692dc9b4
lesliechen <435421355@qq.com>
2022-05-17 21:45:43 +0800

    feat:n的阶乘中0的个数

--- day3.js
+++ day3.js
@@ -1,3 +1,22 @@
 /**
- *  n的阶乘中0的个数
+ *  题目：给定一个整数N，那么N的阶乘N！末尾有多少个0呢？
+    例如：N＝10，N！＝3 628 800，N！的末尾有两个0
  */
+
+    function factorial(n) {
+      let count = 0;
+      while(n > 0) {
+        n = parseInt(n / 5);
+        count += n;
+      }
+      return count
+    }
+    
+    const res = factorial(5)
+    const res1 = factorial(3)
+    const res2 = factorial(8)
+    const res3 = factorial(10)
+    console.log(res)
+    console.log(res1)
+    console.log(res2)
+    console.log(res3)
