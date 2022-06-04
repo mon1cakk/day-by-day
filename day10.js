c167a673662196bce2b23bdcbb65eead3e7cf2c7
lesliechen <435421355@qq.com>
2022-06-04 23:57:48 +0800

    feat:计算对象的层级

--- day10.js
+++ day10.js
@@ -18,3 +18,38 @@
     }
   };
  */
+
+  function getLevel(obj = {}) {
+    let result = 1;
+    const dfs = (param, level = 0) => {
+      if(typeof param === 'object' && param !== null) {
+        Object.values(param).forEach(item => {
+          if(typeof item === 'object' && item !== null) {
+            dfs(item, level+1);
+          }else {
+            result = level + 1 > result? level + 1: result;
+          }
+        })
+      }else {
+        result = level > result? level: result;
+      }
+    }
+    dfs(obj);
+    return result
+  }
+  const obj = {
+    a: 'a',
+    b: {
+      c: {
+        d: 'd',
+        e: 'e',
+      }
+    },
+    f: {
+      g: {
+        h: {
+          i: 'i',
+        }
+      },
+    }
+  };
