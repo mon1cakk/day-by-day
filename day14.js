ebd8550f83328c3e2054a841ac58ad6ab4bc56db
lesliechen <435421355@qq.com>
2022-06-10 01:50:11 +0800

    feat:找出1-n中1出现的次数

--- day14.js
+++ day14.js
@@ -3,3 +3,15 @@
  * 1 - 10 [1, 10] => 2
  * 1 - 20 [1 10 11 12 13 14...19] => 12
  */
+
+function numberOneCount(n) {
+  return Array.from({ length: n })
+  .map((v, i) => i + 1)
+  .reduce((pre, cur) => {
+    // const curCount = String(cur).match(/1/g)?.length || 0;
+    const curCount = String(cur).split('').filter(i => i === '1').length;
+    return pre + curCount;
+  }, 0);
+}
+console.log(numberOneCount(10));
+console.log(numberOneCount(20));
