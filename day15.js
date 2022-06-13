73b8a0f1449513b16fe13dc6c3f634a1c56efd87
lesliechen <435421355@qq.com>
2022-06-13 23:56:39 +0800

    feat:promise实现红绿灯

--- day15.js
+++ day15.js
@@ -1,3 +1,21 @@
 /**
  * Promise实现一个红绿灯
  */
+
+function timer(color, timer) {
+  return new Promise((resolve, reject) => {
+    console.log(color);
+    setTimeout(() => {
+      resolve();
+    }, timer * 1000)
+  })
+}
+
+async function light() {
+  await timer('red', 5);
+  await timer('green', 3);
+  await timer('yellow', 2);
+  await light();
+}
+
+light()
