120c9a0c89a277784cacfbf4bfe9e86d763362c5
Lesliechen <chenwenlong@LesliechendeMacBook-Pro.local>
2022-06-26 23:14:57 +0800

    装环境永远是一生之敌

---
+++ day23.js
@@ -0,0 +1,17 @@
+/*
+ * @Author: LeslieChen 
+ * @Date: 2022-06-23 08:15:43 
+ * @Last Modified by: LeslieChen
+ * @Last Modified time: 2022-06-23 08:22:30
+ */
+
+// 字符串全排列
+/**
+ * 示例1
+ * 输入: abc
+ * 输出: abc acb bac bca cab cba
+ * 
+ * 示例2
+ * 输入: abcd
+ * 输出: abcd abdc acbd acdb adbc adcb ... dcba -> 共计24个值
+ */
