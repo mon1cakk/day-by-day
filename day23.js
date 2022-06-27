95029f4170e6634bd6ee1a72e4c740b78eb6932e
chenwenlong7 <435421355@qq.com>
2022-06-27 23:47:05 +0800

    feat:字符串全排列

--- day23.js
+++ day23.js
@@ -2,7 +2,7 @@
  * @Author: LeslieChen 
  * @Date: 2022-06-23 08:15:43 
  * @Last Modified by: LeslieChen
- * @Last Modified time: 2022-06-23 08:22:30
+ * @Last Modified time: 2022-06-27 23:46:01
  */
 
 // 字符串全排列
@@ -15,3 +15,28 @@
  * 输入: abcd
  * 输出: abcd abdc acbd acdb adbc adcb ... dcba -> 共计24个值
  */
+
+ function swap(arr, i, j) {
+  if (i !== j) {
+    const tmp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = tmp;
+  }
+}
+ 
+function perm(arr) {
+  function fn(n) {
+    for (let i = n; i < arr.length; i++) {
+      swap(arr, i, n);
+      if (n + 1 < arr.length - 1) {
+        fn(n + 1);
+      } else {
+        console.log(arr);
+      }
+      swap(arr, i, n);
+    }
+  }
+  fn(0);
+}
+const res = perm(['a', 'b', 'c', 'd']);
+console.log(res);
