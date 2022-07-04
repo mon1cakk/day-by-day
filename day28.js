6a962421cece4ccdc67763ac7cbcf58525844d3e
chenwenlong7 <435421355@qq.com>
2022-07-04 23:56:19 +0800

    动态插入10000个li标签

---
+++ day28.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: LeslieChen 
+ * @Date: 2022-07-03 18:00:24 
+ * @Last Modified by: LeslieChen
+ * @Last Modified time: 2022-07-04 23:55:40
+ */
+
+//原生实现一个表单提交
+function formPost(url, params) {
+  const temp = document.createElement('form');
+}
+//动态插入10000个li标签
