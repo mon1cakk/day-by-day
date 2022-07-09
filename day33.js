7c78680daab2eb3186e9739bcc376460dd9315a3
chenwenlong7 <435421355@qq.com>
2022-07-09 16:44:27 +0800

    feat:实现一个compose函数

---
+++ day33.js
@@ -0,0 +1,33 @@
+/*
+ * @Author: LeslieChen 
+ * @Date: 2022-07-09 16:14:31 
+ * @Last Modified by: LeslieChen
+ * @Last Modified time: 2022-07-09 16:43:42
+ */
+
+//实现一个compose函数
+// 用法如下:
+function fn1(x) {
+  return x + 1;
+}
+function fn2(x) {
+  return x + 2;
+}
+function fn3(x) {
+  return x + 3;
+}
+function fn4(x) {
+  return x + 4;
+}
+const a = compose(fn1, fn2, fn3, fn4);
+console.log(a(1)); // 1+4+3+2+1=11
+
+function compose(...fn) {
+  if (!fn.length) return (v) => v;
+  if (fn.length === 1) return fn[0];
+  return fn.reduce(
+    (pre, cur) =>
+      (...args) =>
+        pre(cur(...args))
+  );
+}
