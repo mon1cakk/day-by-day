20267036f45c25ae9c7338965c7dc0c0033022e0
chenwenlong7 <435421355@qq.com>
2022-07-14 22:44:31 +0800

    feat:实现一个sleep函数

---
+++ day38.js
@@ -0,0 +1,32 @@
+/*
+ * @Author: LeslieChen 
+ * @Date: 2022-07-14 22:31:44 
+ * @Last Modified by: LeslieChen
+ * @Last Modified time: 2022-07-14 22:36:37
+ */
+
+//实现一个sleep函数
+const sleep = (func, delay) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(func())
+    }, delay)
+  })
+}
+
+const consoleStr = (str) => {
+  return () => {
+    console.log(str);
+    return str
+  }
+}
+
+const doFns = async () => {
+  const name = await sleep(consoleStr('前端胖头鱼'), 1000)
+  const sex = await sleep(consoleStr('boy'), 1000)
+  const age = await sleep(consoleStr(100), 1000)
+
+  console.log(name, sex, age)
+}
+
+doFns()
