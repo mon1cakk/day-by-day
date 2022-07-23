18fde2ca45d363bf4cffdf90674d723f61434e5e
chenwenlong7 <435421355@qq.com>
2022-07-24 00:23:54 +0800

    feat:寻找合适的开会时间

--- day44.js
+++ day44.js
@@ -2,7 +2,7 @@
  * @Author: LeslieChen 
  * @Date: 2022-07-23 01:03:29 
  * @Last Modified by: LeslieChen
- * @Last Modified time: 2022-07-23 02:34:56
+ * @Last Modified time: 2022-07-24 00:23:02
  */
 
 // 寻找合适开会的时间
@@ -28,37 +28,64 @@ const timesArr = [
   [[8, 9]], //成员2的安排
   [[13, 18]] //成员3的安排
 ]
-function findMeetingSlots(timesArr) {
-  const startTime = 0;
-  const endTime = 24;
-  const result = [];
-  timesArr.forEach((item) => {
-    item.sort((a, b) => a[0] - b[0])
+// function findMeetingSlots(timesArr) {
+//   const startTime = 0;
+//   const endTime = 24;
+//   const result = [];
+//   timesArr.forEach((item) => {
+//     item.sort((a, b) => a[0] - b[0])
+//   })
+//   timesArr.forEach((item) => {
+//     const temp = [];
+//     for(let i = 0; i < item.length; i++) {
+//       if(item[i][0] > startTime && i === 0) {
+//         temp.push([startTime, item[i][0]])
+//       }
+//       if(i > 0 && i < item.length - 1) {
+//         if(item[i + 1][0] > item[i][1]) {
+//           temp.push([item[i][1], item[i + 1][0]])
+//         }
+//       }
+//       if(i === item.length - 1 && item[i][1] < endTime) {
+//         temp.push([item[i][1], endTime])
+//       }
+//     }
+//     result.push(temp);
+//   })
+//   return result
+// }
+// // 
+// const intersectNoRepeatTwice = arrs => {
+//   return arrs.reduce(function(prev,cur){
+//       // return [...new Set(cur.filter((item)=>prev.includes(item)))]
+//       return Array.from(new Set(cur.filter((item)=>prev.includes(item))))
+//   })
+// }
+
+const findMeetingSlots = (planTimes) => {
+  const schedule = new Array(24).fill(0);
+  planTimes.flat(1).forEach(item => {
+    schedule.fill(1, item[0], item[1]);
   })
-  timesArr.forEach((item) => {
-    const temp = [];
-    for(let i = 0; i < item.length; i++) {
-      if(item[i][0] > startTime && i === 0) {
-        temp.push([startTime, item[i][0]])
-      }
-      if(i > 0 && i < item.length - 1) {
-        if(item[i + 1][0] > item[i][1]) {
-          temp.push([item[i][1], item[i + 1][0]])
-        }
-      }
-      if(i === item.length - 1 && item[i][1] < endTime) {
-        temp.push([item[i][1], endTime])
-      }
+  let start = 0, end = 0, lens = schedule.length, result = [];
+  while (end < lens) {
+    if (schedule[start] === 1 && schedule[end] === 0) { // [1,0], reset start index, incre end
+      start = end;
+      end++;
+      continue;
     }
-    result.push(temp);
-  })
-  return intersectNoRepeatTwice([...new Set(result.flat())])
+    if (schedule[end] === 1) { // meet end tag
+      if (schedule[start] === 0) { // [0, ..., 1]
+        result.push([start, end]);
+      }
+      start = end; // [0, ..., 1] [1, ..., 1], update start index
+    }
+    end++;
+  }
+  if (end === lens && end - start >= 1) {
+    result.push([start, end]);
+  }
+  return result;
 }
-// 
-const intersectNoRepeatTwice = arrs => {
-  return arrs.reduce(function(prev,cur){
-      // return [...new Set(cur.filter((item)=>prev.includes(item)))]
-      return Array.from(new Set(cur.filter((item)=>prev.includes(item))))
-  })
-}
+
 console.log('787', findMeetingSlots(timesArr))
